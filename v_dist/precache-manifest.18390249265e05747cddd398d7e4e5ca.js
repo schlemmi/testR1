77b069923c8b8aa51dbe85519030951acbcbdc09
schlemmi <alastairyoung@yahoo.co.uk>
2020-03-31 20:36:40 +0900

    fix missing lib

---
+++ v_dist/precache-manifest.18390249265e05747cddd398d7e4e5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ab9a6a144eda7665ef",
+    "url": "/css/app.6f6fffa0.css"
+  },
+  {
+    "revision": "4b4c5d6731912b906f07e48991637d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ab9a6a144eda7665ef",
+    "url": "/js/app.fa3b9099.js"
+  },
+  {
+    "revision": "87b8b9a4b7db59b963f5",
+    "url": "/js/chunk-vendors.67946602.js"
+  },
+  {
+    "revision": "9d06ce5c7f50fdff022c3fcf48952b4b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
